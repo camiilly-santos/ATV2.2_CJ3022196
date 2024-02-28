b0d9c1f5bdaabd02549b055f63fbbb6b666aefb1
CJ3022196 <CJ3022196@LAB01-PC39>
2024-02-28 09:59:11 -0300

    Adicionar arquivos de projeto.

---
+++ ATV2.2_CJ3022196/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main ()
+    {
+
+        Console.WriteLine("Digite um número inteiro: ");
+        int numero =
+        Convert.ToInt32(Console.ReadLine());
+
+        int resultado = (numero * 3 + 1) + (numero * 2 - 1);
+
+        Console.WriteLine(" A soma do sucessor do triplo com o antecessor do dobro é: " + resultado);
+    }
+}
